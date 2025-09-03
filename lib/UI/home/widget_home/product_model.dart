b5d0139f03e7f2ce0a27f57c121a542e36deaff6
martinawageeh <martinawageeh038@gmail.com>
2025-09-03 17:48:51 +0300

    % test project

---
+++ lib/UI/home/widget_home/product_model.dart
@@ -0,0 +1,13 @@
+class Product {
+  final String imagePath;
+  final String name;
+  final String rate;
+  final String price;
+
+  Product({
+    required this.imagePath,
+    required this.name,
+    required this.rate,
+    required this.price,
+  });
+}
