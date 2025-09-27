399e1690995b8884058e826ede538ec2c09ff1b2
MarianMakram <marianmakram303@gmail.com>
2025-09-27 09:19:30 +0300

    % delivery Address

--- lib/UI/checkout/widgets/delivery_payment.dart
+++ lib/UI/checkout/widgets/delivery_payment.dart
@@ -5,7 +5,6 @@ import "optionTile.dart";
 
 class DeliveryAndPayment extends StatelessWidget {
   const DeliveryAndPayment({super.key});
-
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
