839a89803a614c3369d6a503f623b1a85447742c
PHLY101 <basel6889@gmail.com>
2025-09-29 11:21:32 +0300

    + organized code via barrel imports

---
+++ lib/UI/ui.dart
@@ -0,0 +1,23 @@
+// This is a barrel import file.
+
+
+// Auth
+export "auth/auth.dart";
+
+
+// Drawer & Settings
+export "Settings/drawer/drawer.dart";
+
+
+// Tabs & Screens
+export "home/home_tab.dart";
+export "Cart/view/cart_page.dart";
+export "Search/search_tab.dart";
+export "Profile/profile_tab.dart";
+export "Payment/payment_screen.dart";
+export "Schedule/schedule_screen.dart";
+export "Summary/summary_screen.dart";
+export "checkout/checkout_screen.dart";
+export "AboutScreen/about_screen.dart";
+export "main_app/main_screen.dart";
+export "SplashScreen/splash_screen.dart";
