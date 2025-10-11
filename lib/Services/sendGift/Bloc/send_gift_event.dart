6b021c80cc76bd7d7e9918bbef8ff5d31aefd1b4
PHLY101 <basel6889@gmail.com>
2025-10-11 13:51:46 +0300

    + init the sendGift bloc

---
+++ lib/Services/sendGift/Bloc/send_gift_event.dart
@@ -0,0 +1,4 @@
+part of 'send_gift_bloc.dart';
+
+@immutable
+sealed class SendGiftEvent {}
---
+++ lib/Services/sendGift/Bloc/send_gift_state.dart
@@ -0,0 +1,6 @@
+part of 'send_gift_bloc.dart';
+
+@immutable
+sealed class SendGiftState {}
+
+final class SendGiftInitial extends SendGiftState {}
